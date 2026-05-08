2da22bcd2c1da8cbd2762811d17c758d41a765be
Warren <warrensauro15@gmail.com>
2026-05-08 18:54:51 +0800

    data_analyzer: renames some files and fixed the loader

--- CLI/data_analyzer_CLI/csv_loader.py
+++ CLI/data_analyzer_CLI/csv_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
-
+import os
 def load_csv(file_path):
-    try:  
-        dataframe = pd.read_csv(file_path)
-        return dataframe
-    except FileNotFoundError:
+    if not os.path.exists(file_path):
         raise FileNotFoundError(f"No file found at: {file_path}")
-    
+    try:  
+        return pd.read_csv(file_path)
+    except (pd.errors.ParserError, UnicodeDecodeError) as e:
+        raise ValueError(f"Corrupted data in {file_path}") from e
